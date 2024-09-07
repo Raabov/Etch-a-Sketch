302c4ae44c75c348cf7eeed09a44c15cea59a7d0
blackcat <97760591+Raabov@users.noreply.github.com>
2024-09-07 07:11:12 -0400

    Added some style to divs in the DOM with js

--- etchscript.js
+++ etchscript.js
@@ -1,6 +1,12 @@
-let container = document.querySelector(".container");
+let container = document.querySelector('.container');
 
 for (let i = 0 ; i < 16; i++) {
     let node = document.createElement("div");
+    
+    node.style.backgroundColor = 'pink';
+    node.style.width = '50px';
+    node.style.height = '50px';
+    node.style.outline = '2px solid red';
+
     container.appendChild(node);
 }
