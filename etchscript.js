89c195372996a45f50713474d4e873b37bef8bd7
blackcat <97760591+Raabov@users.noreply.github.com>
2024-09-07 11:10:16 -0400

    Changed size of divs to 20px

--- etchscript.js
+++ etchscript.js
@@ -3,8 +3,8 @@ let container = document.querySelector('.container');
 
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.width = '1600px';
-container.style.heigh = '1600px';
+container.style.width = '320px';
+container.style.heigh = '320px';
 
 // Loop to create grid of 16*16
 // Appended to container
@@ -13,8 +13,8 @@ for (let i = 0 ; i < 256; i++) {
     let node = document.createElement("div");
     
     node.style.backgroundColor = 'pink';
-    node.style.width = '50px';
-    node.style.height = '50px';
+    node.style.width = '20px';
+    node.style.height = '20px';
     node.style.outline = '2px solid red';
 
     container.appendChild(node);
