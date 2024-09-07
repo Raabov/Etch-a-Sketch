634e8173489efd58bd316270deca3df8aa1f5cc0
blackcat <97760591+Raabov@users.noreply.github.com>
2024-09-07 07:21:32 -0400

    Added comments to javascript

--- etchscript.js
+++ etchscript.js
@@ -1,8 +1,13 @@
+//Selected container and added style to the DOM
 let container = document.querySelector('.container');
+
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 container.style.width = '1600px';
 container.style.heigh = '1600px';
+
+// Loop to create grid of 16*16
+// Appended to container
 
 for (let i = 0 ; i < 256; i++) {
     let node = document.createElement("div");
