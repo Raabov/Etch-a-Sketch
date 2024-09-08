037096a6b0bd05e9150aec42aa01f3c95d5e9f15
blackcat <97760591+Raabov@users.noreply.github.com>
2024-09-08 10:37:10 -0400

    Cleared existing grid

--- etchscript.js
+++ etchscript.js
@@ -1,31 +1,40 @@
-//Selected container and added style to the DOM
-let container = document.querySelector('.container');
+    //Selected container and added style to the DOM
+    let container = document.querySelector('.container');
+    let choice = document.querySelector('.choice');
+    let size = 0;
+
+    function userChoice(){
+        //Clear existing grid items
+        container.innerHTML = '';
+
+        size = prompt(`Enter grid size less than 16:`);
+        
+        let itemSize = 320 / size;  
+
+        container.style.width = `${itemSize * size}px`;
+        container.style.height = `${itemSize * size}px`;
+
+        for (let i = 0 ; i < size*size; i++) {
+            let node = document.createElement("div");
+            node.className = 'container-item';
+            
+            node.style.backgroundColor = 'pink';
+            node.style.width = `${itemSize}px`;
+            node.style.height = `${itemSize}px`;
+            node.style.outline = '2px solid red';
+        
+            node.addEventListener('mouseover', function() {
+                this.style.backgroundColor = 'blue';
+            });
+        
+            node.addEventListener('mouseout', function() {
+                this.style.backgroundColor = 'lightblue'
+            });
+        
+            container.appendChild(node);
+        }
+    }
+    choice.addEventListener('click', userChoice);
 
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.width = '320px';
-container.style.heigh = '32 0px';
-
-// Loop to create grid of 16*16
-// Appended to container
-
-for (let i = 0 ; i < 256; i++) {
-    let node = document.createElement("div");
-    node.className = 'container-item';
-    
-    node.style.backgroundColor = 'pink';
-    node.style.width = '20px';
-    node.style.height = '20px';
-    node.style.outline = '2px solid red';
-
-    node.addEventListener('mouseover', function() {
-        this.style.backgroundColor = 'blue';
-    });
-
-    node.addEventListener('mouseout', function() {
-        this.style.backgroundColor = 'lightblue'
-    });
-
-    container.appendChild(node);
-}
-
